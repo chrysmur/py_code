bd4fc46616dc294a86148c689aaf5c4650c4d396
chrysmur <sostomc011@gmail.com>
2021-03-13 22:44:03 +0300

    oop properties

--- abstraction.py
+++ abstraction.py
@@ -8,51 +8,105 @@
 # There should be only one way to change the attributes
 
 class Robot:
+    #Class attributes
+    Three_Laws = (
+    """A robot may not injure a human being""",
+    """A robot must obey the orders given to it by human beings""",
+    """A robot must protect humans then its own existence""")
+    __counter = 0
+    __instance = None
+
     def __init__(self,
                  name=None,
                  build_year=None):
-        self.name = name
-        self.build_year = build_year
+        self.__name = name
+        self.__build_year = build_year
+        #Count the  instances of this class type(self) evaluates to Robot
+        type(self).__counter += 1
+        type(self).__instance = self
+
+    def __del__(self):
+        type(self).__counter -= 1
+    
+    @classmethod
+    def robot_instances(cls):
+        return cls, Robot.__counter # Access class attr using the class
+    
+    # class method used where static methods have to call other static methods
+    # instead of hardcoding the class name
+    @classmethod
+    def get_instance(cls):
+        return cls.__instance
+
+    @staticmethod
+    def robot_creator():
+        return "By Sabonis"
 
     def use_name(self):
-        if self.name:
-            print(f"{self.name} is available")
+        if self.__name:
+            print(f"{self.__name} is available")
         else:
             print("This class has no attribute name")
 
-        if self.build_year:
-            print(f"last update in {self.build_year}")
+        if self.__build_year:
+            print(f"last update in {self.__build_year}")
         else:
             print(f"year of creation unknown")
 
     def set_name(self, name):
-        self.name = name
+        self.__name = name
 
     def get_name(self):
-        return self.name
+        return self.__name
 
     def set_build_year(self, year):
-        self.build_year = year
+        self.__build_year = year
 
     def get_build_year(self):
-        return self.build_year
+        return self.__build_year
 
     def __repr__(self):
-        return f"Class name {self.name}"
+        return self.__name
+    
+    def __str__(self):
+        return self.__name
+
+class Pet:
+    _class_info = "pet animals"
+    #by defining this method as a regular method
+    #Class that import it and redefine _class_info will have their own displayed
+    # def about(self):
+    #     return "This calss is about " + self._class_info + "!"
+    
+    # making it static will ensure imports only display _class_info of imported
+    # @staticmethod
+    # def about():
+    #     return "This class is about " + Pet._class_info + "!"
+
+    #making it a class method imply that _class_info in inheriting classes 
+    # will access about with their own class name and the new classinfo
+    @classmethod
+    def about(cls):
+        return f"Class about {cls._class_info}"
+class Dog(Pet):
+    _class_info =  "man's best friend"
+
+
+class Cat(Pet):
+    _class_info = "All kinds of cats"
+
+class Racoon(Pet):
+    pass
 
 
 if __name__ == "__main__":
-    # instantiate class
-    robot = Robot('optimus 1', '2065')
+    
+    p = Pet()
+    d = Dog()
+    c = Cat()
+    r = Racoon()
 
-    # do we have attribute name?
-    robot.use_name()
-
-    # set name
-    robot.set_name("new name")
-
-    # Use name again
-    robot.get_build_year()
-
-    # Get name
-    print(robot.get_name())
+    print(p.about())
+    print(d.about())
+    print(c.about())
+    print(r.about())
